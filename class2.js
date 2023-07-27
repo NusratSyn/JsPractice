6032085fea89aab32e706695fc4fef6ac1c162b7
NusratSyn <synthu143@gmail.com>
2023-07-27 13:36:39 +0600

    object class

---
+++ class2.js
@@ -0,0 +1,19 @@
+let stringType = "Joshim";
+let numberType = 7;
+let booleanType = false;
+
+let objectType = {
+    'student1': {'name':'Kim Namjoon', 'class':10, 'roll':1, 'address':{'country':'SK','city':'Seoul','road':4,'house':314}, 'guardian':{'name':'kim shin','relation with student':'uncle'}},
+    'student2': {'name':'Kim Seokjin', 'class':9, 'roll':1, 'address':{'country':'SK','city':'Seoul','road':1,'house':314}, 'guardian':{'name':'kim shin','relation with student':'uncle'}},
+    'student3': {'name':'Min Yoongi', 'class':9, 'roll':10, 'address':{'country':'SK','city':'Deogu','road':2,'house':514}, 'guardian':{'name':'So hee','relation with student':'sister'}},
+    'student4': {'name':'Jung Hoseok', 'class':9, 'roll':2, 'address':{'country':'SK','city':'Busan','road':1,'house':1}, 'guardian':{'name':'lee min ho','relation with student':'Brother'}},
+    'student5': {'name':'Park Jimin', 'class':8, 'roll':2, 'address':{'country':'SK','city':'Busan','road':314,'house':2}, 'guardian':{'name':'lee min ho','relation with student':'Brother'}},
+    'student6': {'name':'Kim Teahuyng', 'class':8, 'roll':12, 'address':{'country':'SK','city':'Deogu','road':4,'house':2}, 'guardian':{'name':'Kim jin','relation with student':'Brother'}},
+    'student7': {'name':'Jeon Jungkook', 'class':7, 'roll':1, 'address':{'country':'SK','city':'Busan','road':27,'house':27}, 'guardian':{'name':'Bang PD','relation with student':'Father'}},
+    'student8': {'name':'Rose', 'class':9, 'roll':1, 'address':{'country':'Australia','city':'Melbourne','road':2,'house':1}, 'guardian':{'name':'lisa','relation with student':'Sister'}},
+    'student9': {'name':'Lisa', 'class':10, 'roll':2, 'address':{'country':'Australia','city':'Melbourne','road':2,'house':1}, 'guardian':{'name':'Rose','relation with student':'Sister'}},
+    'student10': {'name':'Jisoo', 'class':10, 'roll':3, 'address':{'country':'Australia','city':'Melbourne','road':2,'house':1}, 'guardian':{'name':'Lisa','relation with student':'Sister'}},
+} /* this is a database of 10 students. 
+there name, roll, country, address and guardian informations are given here.*/
+
+console.log(objectType);
