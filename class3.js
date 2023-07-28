0d1c486a9eb69bbd5df7bfc5b86d07d8365ef5f4
NusratSyn <synthu143@gmail.com>
2023-07-28 15:17:18 +0600

    class 3 project

---
+++ class3.js
@@ -0,0 +1,26 @@
+const totalNum=100;
+
+if (totalNum>=90){
+    console.log('A+','passed');
+}
+else if (totalNum>=80){
+    console.log('A-','passed');
+}
+else if (totalNum>=70){
+    console.log('B+','passed');
+}
+else if (totalNum>=60){
+    console.log('B-','passed');
+}
+else if (totalNum>=50){
+    console.log('C','passed');
+}
+else if (totalNum>=40){
+    console.log('D','passed');
+}
+else if (totalNum>=33){
+    console.log('passed');
+}
+else {
+    console.log('failed');
+}
